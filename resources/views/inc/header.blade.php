a2ba0d77839ff7d65487c7299323cf41a3d1f3e5
hungry_la <hungry_la@mail.ru>
2021-07-15 23:15:07 +0500

    Изменил проблему того, что на мобилке ссылке в шапке пропадали

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -3,19 +3,21 @@ if(auth()->user()){
     $nameOfUser = auth()->user()->name;
 };
 ?>
-<header class="site-header sticky-top py-1">
-    <nav class="navbar navbar-expand-lg ">
-        <a class="navbar-brand" href="{{route('home')}}">BookStore</a>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto grid">
-                <li class="nav-item active">
+<header class="site-header sticky-top">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="{{route('home')}}">BookStore</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     @if(auth()->user())
-                        <div class="dropdown">
-                            <a class="btn btn-secondary dropdown-toggle" href="#"
-                               role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                        <div class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
                                 {{$nameOfUser}}
                             </a>
-                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                 @if(auth()->user()->is_admin == 1)
                                     <li><a class="dropdown-item" href="{{route('adminMain')}}">Админка</a></li>
                                 @endif
@@ -23,35 +25,31 @@ if(auth()->user()){
                             </ul>
                         </div>
                     @endif
-                </li>
-                <li class="nav-item active">
+                </ul>
+                <div class="d-flex">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    @guest()
+                        <li class="nav-item active">
+                            <a class="nav-link" href="{{route('basket')}}">Корзина</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="{{route('login')}}">Войти</a>
+                        </li>
 
-                </li>
-                <li class="nav-item active">
+                    @endguest
 
-                </li>
+                    @auth()
 
-                @guest()
-                    <li class="nav-item active">
-
-                    </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="{{route('basket')}}">Корзина</a>
-                    </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="{{route('login')}}">Войти</a>
-                    </li>
-                @endguest
-
-                @auth()
-                    <li class="nav-item active" >
-                        <a class="nav-link" href="{{route('editDB')}}">Добавить книгу</a>
-                    </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="{{route('basket')}}">Корзина</a>
-                    </li>
-                @endauth
-            </ul>
+                        <li class="nav-item active" >
+                            <a class="nav-link" href="{{route('editDB')}}">Добавить книгу</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="{{route('basket')}}">Корзина</a>
+                        </li>
+                    @endauth
+                    </ul>
+                </div>
+            </div>
         </div>
     </nav>
     {{--<nav class="container d-flex flex-column flex-md-row justify-content-between">
